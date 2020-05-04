cdc34eac41314f22c84052d142593a4a2e901bc4
JoycePer <60732770+JoycePer@users.noreply.github.com>
2020-05-04 12:42:23 +0200

    Add files via upload

---
+++ Phase_One_Cities.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Phase_One_Cities {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		String cityOne= sc.nextLine();
+		String cityTwo= sc.nextLine();
+		String cityThree= sc.nextLine();
+		String cityFour= sc.nextLine();
+		String cityFive= sc.nextLine();
+		String citySix= sc.nextLine();
+		
+		
+		System.out.print(cityOne);
+			
+		}
+		
+		
+
+	}
+
